8edda4a1e601bbb6ef2af6c69311bcf31d717bb9
susmit-singh <singh.susmit@gmail.com>
2017-02-25 19:40:28 +0545

    added files on 25th feb

---
+++ TAAHO/LandingPageBottomUIView.swift
@@ -0,0 +1,21 @@
+//
+//  LandingPageBottomUIView.swift
+//  TAAHO
+//
+//  Created by Susmit Singh on 2/21/17.
+//  Copyright © 2017 Susmit Singh. All rights reserved.
+//
+
+import UIKit
+
+class LandingPageBottomUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ TAAHO/SearchUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SearchUIView.swift
+//  TAAHO
+//
+//  Created by Susmit Singh on 2/21/17.
+//  Copyright © 2017 Susmit Singh. All rights reserved.
+//
+
+import UIKit
+
+class SearchUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
